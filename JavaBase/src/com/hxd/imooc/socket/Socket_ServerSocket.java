9b1e28bcc99c7ee0fdf47939b9a69cd18c338895
hxd <1064209914@qq.com>
2017-04-11 23:29:27 +0800

    add socket

--- JavaBase/src/com/hxd/imooc/socket/Socket_ServerSocket.java
+++ JavaBase/src/com/hxd/imooc/socket/Socket_ServerSocket.java
@@ -4,39 +4,46 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- *基于TCP协议的socket的通信 实现用户登录
- *服务端
+ * 基于TCP协议的socket的通信 实现用户登录 服务端
  */
 public class Socket_ServerSocket {
 
 	public static void main(String[] args) {
 		try {
-			//1.创建一个服务端socket,即serversocket，并绑定端口  监听指定端口
-			ServerSocket serverSocket=new ServerSocket(8888);
-			//2.调用accept方法 来监听此端口  	  等待客户连接
-			Socket socket=serverSocket.accept();
-			//3.获取输入流	 并读取客户端信息
-			InputStream inputStream=socket.getInputStream();//用socket的方法 
-			InputStreamReader isr=new InputStreamReader(inputStream);
-			BufferedReader br=new BufferedReader(isr);
-			//循环读取客户端信息
-			String s=null;
+			// 1.创建一个服务端socket,即serversocket，并绑定端口 监听指定端口
+			ServerSocket serverSocket = new ServerSocket(8888);
 			System.out.println("*****服务器端即将启动*****");
-			while((s=br.readLine())!=null){
-				System.out.println("客户端输入的信息："+s);
+			// 2.调用accept方法 来监听此端口 等待客户连接
+			Socket socket = serverSocket.accept();
+			// 3.获取输入流 并读取客户端信息
+			InputStream inputStream = socket.getInputStream();// 用socket的方法
+			InputStreamReader isr = new InputStreamReader(inputStream);
+			BufferedReader br = new BufferedReader(isr);
+			// 循环读取客户端信息
+			String s = null;
+			while ((s = br.readLine()) != null) {		//readline 是以\t \n 结束
+				System.out.println("客户端输入的信息：" + s);
 			}
-			socket.shutdownInput();//关闭输入流
-			//4.关闭资源
+			//socket.shutdownInput();// 关闭输入流
+			//获取输出流  用socket
+			OutputStream os=socket.getOutputStream();
+			PrintWriter pw=new PrintWriter(os);
+			pw.println("欢迎您！");
+			pw.flush();
+			pw.close();
+			// 4.关闭资源
 			inputStream.close();
 			isr.close();
 			br.close();
 			socket.close();
 			serverSocket.close();
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- JavaBase/src/com/hxd/imooc/socket/Socket_clinet.java
+++ JavaBase/src/com/hxd/imooc/socket/Socket_clinet.java
@@ -1,6 +1,9 @@
 package com.hxd.imooc.socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -14,15 +17,25 @@ public class Socket_clinet {
 
 	public static void main(String[] args) {
 		try {
-			//1创建客户端socket	指定服务器和端口
-			Socket socket=new Socket("localhost", 8888);
-			//2.获取输出流 像服务器端发送信息
-			OutputStream os=socket.getOutputStream();//字节流
-			PrintWriter pW=new PrintWriter(os);//将输出流包装成打印流输出
-			pW.write("黄敏 吊！");
+			// 1创建客户端socket 指定服务器和端口
+			Socket socket = new Socket("localhost", 8888);
+			// 2.获取输出流 像服务器端发送信息
+			OutputStream os = socket.getOutputStream();// 字节流
+			PrintWriter pW = new PrintWriter(os);// 将输出流包装成打印流输出
+			pW.println("黄敏");
+			pW.flush();
+			socket.shutdownOutput();		//用完之后  要关闭 流 不然  他会一直停在这
+			//获取输入流  
+			InputStream is=socket.getInputStream();
+			BufferedReader br=new BufferedReader(new InputStreamReader(is));
+			String s=null;
+			while((s=br.readLine())!=null){
+				System.out.println(s);
+			}
+			is.close();
+			br.close();			
+			// 3. 关闭资源
 			pW.close();
-			//3. 关闭资源
-			os.close();
 			socket.close();
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
@@ -31,7 +44,6 @@ public class Socket_clinet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 
 	}
 
