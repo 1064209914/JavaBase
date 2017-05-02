abe6fcafde23c199434ca74d733be0f29f95cd53
hxd <1064209914@qq.com>
2017-05-03 00:08:56 +0800

    add

--- JavaBase/src/com/hxd/Javabase/common_class/StringPractice.java
+++ JavaBase/src/com/hxd/Javabase/common_class/StringPractice.java
@@ -1,20 +1,33 @@
 package com.hxd.Javabase.common_class;
 
+import org.ietf.jgss.Oid;
+
 public class StringPractice {
 	//查找以hello为靠头的后缀
 	public static void one(){
-		String string="hello.cc;kkk.cc;helo.txt";
+		String string="hello.cc;kkk.cc;hello.txt";
 		String[] strs=string.split(";");
 		for (String str2 : strs) {
+			//判断字符串是否以hello开头
 			if (str2.startsWith("hello")) {
-				
+				//判断最后一个点的索引
+				int num=str2.lastIndexOf(".");
+				//接受后面的字符串
+				String str3=str2.substring((num+1),str2.length());
+				System.out.println(str3);
 			}
 		}
 		
 	}
+	//将传入的字符串首字母大写
+	public static void two(String string){
+		String str=string.substring(0,1).toUpperCase(); //不能用charAt因为 他没有toUpperCase方法
+		String str1=str+string.substring(1,string.length());
+		System.out.println(str1);
+	}
 	public static void main(String[] args) {
-	
-		
+		//one();
+		two("hjkhskjs");
 		
 
 	}
--- JavaBase/src/com/hxd/Javabase/common_class/stringMaths.java
+++ JavaBase/src/com/hxd/Javabase/common_class/stringMaths.java
@@ -2,15 +2,14 @@ package com.hxd.Javabase.common_class;
 
 public class stringMaths {
 	public static void empty(String string) {
-		if (string.length() == 0) {
-			System.out.println("此字符串为空字符串");
-		} else if (string.equals("")) {
-			System.out.println("此字符串为空字符串");
-		} else {
-			System.out.println("此字符串不是空字符串");
+		//判断字符串是否为空
+		if (string.length() == 0||string.trim().length()==0) {
+			System.out.println("为空");
+		}else{
+			System.out.println("非空");
 		}
+			
 	}
-
 	public static void main(String[] args) {
 		char[] cs = { 'j', 'j', 'd', 'a' };
 		System.out.println(new String(cs));
@@ -18,8 +17,12 @@ public class stringMaths {
 		System.out.println(str2.equals(str2.toUpperCase()));
 		System.out.println(str2.equalsIgnoreCase(str2.toUpperCase()));
 		System.out.println(new String(str2.getBytes()));
+		String string2=" a c ";
+		System.out.println(string2.length());
+		System.out.println(string2.trim().length());//去掉头尾的空格 返回字符串
 		empty(str2);
 		empty("");
+		empty(" a");
 	}
 
 }
