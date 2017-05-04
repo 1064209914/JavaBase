0acc02380de87488a12a09593be3fb3eb68c4312
hxd <1064209914@qq.com>
2017-05-05 00:02:56 +0800

    ikk

--- JavaBase/src/com/hxd/Javabase/common_class/DateDemo.java
+++ JavaBase/src/com/hxd/Javabase/common_class/DateDemo.java
@@ -1,13 +1,31 @@
 package com.hxd.Javabase.common_class;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DateDemo {
 
 	public static void main(String[] args) {
-		 Date d= new Date();
-		 System.out.println(d);
-
+		 /*	Calendar c=new GregorianCalendar();
+		 	Date d=c.getTime();
+		 	String dd= new SimpleDateFormat("HH:mm:ss").format(d);
+		 	System.out.println(dd);*/
+		 	//计算7天前；
+		 	Calendar c=new GregorianCalendar();
+		 	//设置成明天24点
+		 	c.add(Calendar.DAY_OF_MONTH, 1);
+		 	c.set(Calendar.HOUR_OF_DAY, 0);
+		 	c.set(Calendar.MINUTE, 0);
+		 	c.set(Calendar.SECOND, 0);
+		 	String  endtime=c.getTime().toLocaleString();
+		 	c.add(Calendar.DAY_OF_MONTH, -7);
+		 	String  starttime=c.getTime().toLocaleString();
+		 	System.out.println(endtime);
+		 	System.out.println(starttime);
+		 	
+		 	
 	}
 
 }
